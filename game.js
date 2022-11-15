6afe41ebb8dcea6501d15d7ec726a1820826d52a
Cailenys <caileny.salazar@gmail.com>
2022-11-15 09:21:55 -0700

    Display Feedback for Correct/Incorrect Answers

--- game.js
+++ game.js
@@ -45,36 +45,48 @@ startGame = () => {
     questionCounter= 0;
     score = 0 ;
     availableQuestions = [...questions];
-    console.log(availableQuestions);
     getNewQuestion();
 };
 
 getNewQuestion = () => {
-    // if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    //     // Go to the end page
-    //     return window.location.assign("/end.html");
-    //   }
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        // Go to the end page
+        return window.location.assign('/end.html');
+    }
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
    
     choices.forEach(choice => {
-        const number = choice.dataset["number"];
-        choice.innerText = currentQuestion["choice" + number];
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
       });
 
     availableQuestions.splice(questionIndex,1);
     acceptingAnswers = true;
-}
+};
+
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
         if(!acceptingAnswers)return;
+
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
-        getNewQuestion();
+        
+    
+        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+        selectedChoice.parentElement.classList.add(classToApply);
+        
+        setTimeout(() =>{
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+        
+        
     });
 });
+
 startGame();
 
